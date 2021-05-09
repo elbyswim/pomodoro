ef4d6edaac6bbcbf532787a374249864d06deef4
Elbert Lai <elbyswim@gmail.com>
2021-05-09 19:49:58 -0400

    Fixed pomodoro. Added pause, resume, and cancel functions.

--- pomodoro.js
+++ pomodoro.js
@@ -2,7 +2,7 @@ const MINUTESPERHOUR = 60;
 const SECONDSPERMINUTE = 60;
 const MILLISECONDSPERSECOND = 1000;
 let timeLeft, endTime, timerInterval;
-let work, short, long, blockLength, blocksLeft, pomodorosLeft;
+let work, short, long, blockLength, blocksLeft, pomodorosLeft, state, paused;
 
 
 function printTimeLeft() {
@@ -31,65 +31,69 @@ function printCounter() {
         "Pomodoros left in this block: " + pomodorosLeft + "."
 }
 
-function printStatus(status) {
-    document.getElementById("status").innerHTML =
-        "Current interval: " + status
+function printState() {
+    let msg;
+    if (state === 'work') {
+        msg = 'Work';
+    } else if (state === 'short') {
+        msg = 'Short Break';
+    } else if (state === 'long') {
+        msg = 'Long Break'
+    } else if (state === 'done') {
+        msg = 'Done'
+    }
+    document.getElementById("state").innerHTML =
+        "Current interval: " + msg;
 }
 
 function pomodoro() {
-    document.getElementById("messages").innerHTML = "Timer Started!";
+    printCounter();
+    printState();
+    if (state === 'done') {
+        setPomodoro();
+        return;
+    }
     document.getElementById("timerButton1").innerHTML = "Pause";
     document.getElementById("timerButton1").setAttribute( "onClick",
         "javascript: pausePomodoro();" );
     document.getElementById("timerButton2").setAttribute( "onClick",
-        "javascript: resetTimer();" );
-    while (blocksLeft > 0) {
-        pomodorosLeft = blockLength;
-        while (pomodorosLeft > 0) {
-            printCounter();
-            endTime = Date.now() + work;
-            console.log('work');
-            printStatus('Work');
-            let flag1 = true;
-            timerInterval = setInterval(function() {
-                if (flag1) {
-                    console.log(1)
-                    flag1 = false
-                }
-                timeLeft = endTime - Date.now();
-                printTimeLeft(timeLeft);
-                if (timeLeft < 0) {
-                    clearInterval(timerInterval);
-                }
-            }, 1);
-            if (pomodorosLeft > 1) {
-                console.log('short');
-                printStatus('Short Break');
-                endTime = Date.now() + short;
-            } else if (blocksLeft > 1) {
-                console.log('long');
-                printStatus('Long Break');
-                endTime = Date.now() + long;
-            }
-            flag1 = true;
-            timerInterval = setInterval(function() {
-                if (flag1) {
-                    console.log(2)
-                    flag1 = false
-                }
-                timeLeft = endTime - Date.now();
-                printTimeLeft(timeLeft);
-                if (timeLeft < 0) {
-                    clearInterval(timerInterval);
-                }
-            }, 1);
-            pomodorosLeft -= 1;
+        "javascript: resetPomodoro();" );
+    if (paused) {
+      paused = false;
+    } else {
+        if (state === 'work') {
+            timeLeft = work;
+        } else if (state === 'short') {
+            timeLeft = short;
+        } else if (state === 'long') {
+            timeLeft = long;
         }
-        blocksLeft -= 1;
-        pomodorosLeft = blockLength;
     }
-    printStatus('No more!');
-    printCounter();
+    endTime = Date.now() + timeLeft;
+    timerInterval = setInterval(function() {
+        timeLeft = endTime - Date.now();
+        printTimeLeft(timeLeft);
+        if (timeLeft < 0) {
+            clearInterval(timerInterval);
+            if (state === 'work') {
+                if (pomodorosLeft > 1) {
+                    state = 'short';
+                    pomodorosLeft -= 1;
+                } else if (blocksLeft > 1) {
+                    state = 'long';
+                    blocksLeft -= 1;
+                    pomodorosLeft = blockLength;
+                } else {
+                    state = 'done';
+                    pomodorosLeft -= 1;
+                    blocksLeft -= 1;
+                }
+            } else {
+                state = 'work'
+            }
+            pomodoro();
+        }
+    }, 1);
 }
 
 function setPomodoro() {
@@ -107,7 +111,7 @@ function setPomodoro() {
     if (work === 0) {
         document.getElementById("messages").innerHTML = "Please set a pomodoro duration.";
         return false;
-    } else if (short === 0) {
+    } else if (short === 0 && blockLength > 1) {
         document.getElementById("messages").innerHTML = "Please set a short break duration.";
         return false;
     } else if (long === 0) {
@@ -120,22 +124,27 @@ function setPomodoro() {
         document.getElementById("messages").innerHTML = "Please set the number of blocks.";
         return false;
     }
+    pomodorosLeft = blockLength;
+    paused = false;
     return true;
 }
 
 function startPomodoro() {
     if (setPomodoro()) {
+        state = 'work';
+        document.getElementById("messages").innerHTML = "Timer Started!";
         pomodoro();
     }
 }
 
 function pausePomodoro() {
     clearInterval(timerInterval);
+    paused = true;
     document.getElementById("messages").innerHTML = "Pomodoro Paused.";
     timeLeft = endTime - Date.now();
-    // document.getElementById("timerButton1").innerHTML = "Resume";
-    // document.getElementById("timerButton1").setAttribute( "onClick",
-    //     "javascript: timer();" );
+    document.getElementById("timerButton1").innerHTML = "Resume";
+    document.getElementById("timerButton1").setAttribute( "onClick",
+        "javascript: pomodoro();" );
     document.getElementById("timerButton2").setAttribute( "onClick",
         "javascript: resetPomodoro();" );
 }
@@ -144,6 +153,11 @@ function resetPomodoro() {
     clearInterval(timerInterval);
     document.getElementById("messages").innerHTML = "Pomodoro cancelled.";
     setPomodoro();
+    printCounter();
+    state = 'done';
+    printState();
+    timeLeft = 0;
+    printTimeLeft();
     document.getElementById("timerButton1").innerHTML = "Start";
     document.getElementById("timerButton1").setAttribute( "onClick",
         "javascript: startPomodoro();" );
